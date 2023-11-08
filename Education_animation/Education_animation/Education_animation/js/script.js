9e6d3fc311beac09d28ef14200c4966b190cbd8b
Kanna <127107049+shiva-sai-kemmasaram@users.noreply.github.com>
2023-11-08 16:32:59 +0530

    Add all files

---
+++ Education_animation/Education_animation/Education_animation/js/script.js
@@ -0,0 +1,38 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+// animated background 
+
+function createBox() {
+
+    let span = document.createElement('span');
+
+    span.classList.add('animated-box');
+
+    let size = Math.random() * 90;
+
+    span.style.height = 40 + size + 'px';
+    span.style.width = 40 + size + 'px';
+
+    span.style.top = Math.random() * innerHeight + 'px';
+    span.style.left = Math.random() * innerWidth + 'px';
+
+    document.querySelector('.gradient-background').appendChild(span);
+
+    setTimeout(() =>{
+        span.remove();
+    },3000)
+
+}
+
+setInterval(createBox, 400);
